ef749f45a01c12dd6da1f09f5e4d1539ee233e23
Jason Walsh <jwalsh@booster.com>
2014-10-30 16:22:27 -0400

    Rails app initialization through Fig
    $ fig run web rails new . --force --database=postgresql --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '1a691703b95472e21eeca6742bd5cb9696c60862b97287c7e53135c69249d96a4d8818087d74f2607c29f56c4533ddd1c72c3a6808a4cfbcba60b87a412fc95e'
